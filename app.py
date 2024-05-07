9c91d1f8b9fdf6ce4e242a308a4f6a60ff2e6c87
taufiqurahman21 <105335421+taufiqurahman21@users.noreply.github.com>
2024-05-07 20:38:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ st.subheader("21082010155")
 
 # Scatter plot
 plt.scatter(data['day'], data['tip'])
-plt.title("Scatter Plot")
+st.write("Scatter Plot")
 plt.xlabel('Day')
 plt.ylabel('Tip')
 st.pyplot(plt)
 
 # Line Plot
-plt.title("Line Plot")
+st.write("Line Plot")
 fig, ax = plt.subplots(figsize=(10, 6))
 line_plot = sns.lineplot(data=data.drop(['total_bill'], axis=1), ax=ax)
 st.pyplot(fig)
